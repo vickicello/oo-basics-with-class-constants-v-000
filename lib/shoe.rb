89f3bd2a5472e1dda7f8fb1bbc77c439be8d29be
Vicki Hamm <vicki.cello@gmail.com>
2018-06-25 02:05:32 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,8 +1,13 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
 
+  BRANDS = []
+  
   def initialize(brand)
+    @brand = brand
+  end
+
+  def brand=(brand)
     @brand = brand
     BRANDS << brand
   end
